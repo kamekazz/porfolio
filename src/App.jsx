0eba0fca309f43ffdc75badb65bc11cf477e0238
kamekazz <taveras78@gmail.com>
2019-01-19 02:06:53 -0800

    lazy loding

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, lazy,Suspense } from 'react';
 import './src/App.scss';
 import Iame from './components/Iame';
 import Fullpage, { FullPageSections, FullpageSection } from '@ap.cx/react-fullpage'
 import AboutMe from './components/aboutMe';
 import OutSkills from './components/OutSkills';
 import  MyProjects  from './components/MyProjects';
+import Loding from './components/Loding';
 
 
 
@@ -26,31 +27,32 @@ class App extends Component {
 
   render() {
     return (
-      <Fullpage>
-      <FullPageSections>
-        <FullpageSection>
-          <Iame  scrollToMyRef={this.scrollToMyRef}   />
-          <div ref={this.myRef}></div>
-        </FullpageSection >
-        <FullpageSection style={{
-          backgroundColor: '#374B4A',
-        }}>
-          <AboutMe  />
-        </FullpageSection>
-        <FullpageSection style={{
-          backgroundColor: '#374B4A',
-        }}>
-        <OutSkills  scrollToMyRef={this.scrollToMyRef}   />
-        </FullpageSection>
-        <FullpageSection style={{
-          backgroundColor: '#374B4A',
-        }}>
-          <MyProjects scrollToMyRef={this.scrollToMyRef} />
-        </FullpageSection>
-      </FullPageSections>
+      <Suspense fallback={<Loding />}>
+          <Fullpage>
+          <FullPageSections>
+            <FullpageSection>
+              <Iame  scrollToMyRef={this.scrollToMyRef}   />
+              <div ref={this.myRef}></div>
+            </FullpageSection >
+            <FullpageSection style={{
+              backgroundColor: '#374B4A',
+            }}>
+              <AboutMe  />
+            </FullpageSection>
+            <FullpageSection style={{
+              backgroundColor: '#374B4A',
+            }}>
+            <OutSkills  scrollToMyRef={this.scrollToMyRef}   />
+            </FullpageSection>
+            <FullpageSection style={{
+              backgroundColor: '#374B4A',
+            }}>
+              <MyProjects scrollToMyRef={this.scrollToMyRef} />
+            </FullpageSection>
+          </FullPageSections>
+        </Fullpage>
+      </Suspense>
 
-    </Fullpage>
-       
     );
   }
 }
