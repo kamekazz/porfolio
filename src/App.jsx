cde1ebe206e9975ef58fd972ef6c7a057a93ad7a
manuel taveras <taveras78@gmail.com>
2019-02-06 22:39:05 -0400

    deseno terminado

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className="app">
         <Nav />
         <Suspense fallback={<Loding />}>
             <App1 />
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 
 
-
+const dataNav=[{title:'About Me',},{title:'My Skills'},{title:'My Projects'},{title:'Web Clone'}]
 
  class Nav extends Component {
 
@@ -13,10 +13,48 @@ import React, { Component } from 'react'
         this.setState({open:!this.state.open})
     }
   render() {
-      const {open}=this.state;
+    const {open}=this.state;
+
+    const rdNavTag =()=>(
+        dataNav.map(item =>(
+            <li key={item.title} className="navItem">{item.title}</li>
+        ))
+    )
+
+
     return (
       <div className={open ? 'Nav ':'Nav close' }>
         <div className="navBar">
+            <div className="contaner menu">
+                <h2 className="h2">Menu</h2>
+            </div>
+            <ul className={open ? 'contaner':'contaner close'}>
+                {rdNavTag()}
+            </ul>
+            <div className="contaner">
+                <a href="https://codepen.io/kamekazz/pens/popular/"rel="noopener noreferrer" target="_blank">
+                    <div  className="gettouch btn">     
+                        <div>
+                            codepen
+                        </div> 
+                    </div>
+                </a>
+                <a href="https://github.com/kamekazz"rel="noopener noreferrer" target="_blank">
+                    <div  className="gettouch btn">     
+                        <div>
+                            more Projects
+                        </div> 
+                    </div>
+                </a>
+                <a href="https://codesandbox.io/u/kamekazz"rel="noopener noreferrer" target="_blank">
+                    <div  className="gettouch btn">     
+                        <div>
+                            codesandbox
+                        </div> 
+                    </div>
+                </a>
+            </div>
+            
             
         </div>
         <div onClick={this.togoleNav} className="small">
