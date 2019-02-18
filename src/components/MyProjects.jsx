9932252085f8a36a7ac26bfb5fa1db08d56df6b6
manuel taveras <taveras78@gmail.com>
2019-02-17 20:46:11 -0400

    fix pro

--- src/components/MyProjects.jsx
+++ src/components/MyProjects.jsx
@@ -8,7 +8,10 @@ import Waypoint from 'react-waypoint';
 
 import Fab from '@material-ui/core/Fab';
 
-
+const data = [
+  {title:'FioBook APP',des:'Is the Dominican Republic most of the people use credit and pay once a month so I created this PWA app to help small business manage account instead of writing everything in aNotebook . To use and demo this app please use this password: 123456 and user account name: TAVERAS78@GMAIL.COM .',code:'',fab:[{link:'https://github.com/kamekazz/fiobook-cli',icon:'fab fa-github'},{link:'https://colbook-0818.firebaseapp.com/login',icon:'fas fa-link'}],tec:['node js','angular','material','bootstrap','mongodb','PWA'],img:'ast/img/mony.jpg'},
+  {title:'Viva la Musica',des:`I created this app for a mall It gives the ability of every guess to add music to a playlist And be able to vote for the next song, This is not the original app I changed a couple things to be suitable for the web .To demo this app please use this password: demo  and user account name: demo.`,code:'',fab:[{link:'https://github.com/kamekazz/viva-cli',icon:'fab fa-github'},{link:'https://viva-la-musica.firebaseapp.com/signin',icon:'fas fa-link'}],tec:['node js','React JS','material','mongodb'],img:'ast/img/music.jpg'}
+]
 
 export class MyProjects extends Component {
 
@@ -31,108 +34,44 @@ export class MyProjects extends Component {
 
 
   render() {
+
+    const rdpro =() =>(
+      data.map(item=>(
+        <div key={item.title} className="card">
+          <div className="top">
+            <figure className="MyProjects__shape--1">
+              <img src={item.img} alt={item.img} className="MyProjects__img" />
+              <figcaption className="MyProjects__caption">{item.title}</figcaption>
+            </figure>
+            <div className="sp">
+              <h2>{item.title}</h2>
+            </div>
+            {item.fab.map(fabI => (
+              <a href={fabI.link} rel="noopener noreferrer" target="_blank"><Fab className="fab" color="primary"  >
+                <i style={{fontSize:'30px'}} className={fabI.icon}></i>
+              </Fab>
+              </a>
+            ))}
+          </div>
+          
+          <p className="des">{item.des}</p>
+          {item.tec.map(tecI => <Chip color="primary" label={tecI} />)}
+          
+        </div>
+      ))
+    )
     const onViw = this.state.onViw
 
     return (
-      <div onScroll={ this.activet} className="MyProjects">
-        <Waypoint
-        onEnter={()=> this.setState({onViw:true})}
-        onLeave={()=> this.setState({onViw:false})}
-        />
-        <figure className="MyProjects__shape--1">
-            <img src="ast/img/mony.jpg" alt="Person on a tour" className="MyProjects__img" />
-            <figcaption className="MyProjects__caption">FioBook APP</figcaption>
-        </figure>
-        <h3 className="MyProjects__h3--1">FioBook APP</h3>
-        <div className="MyProjects__text">
-            <p>
-            Is the Dominican Republic most of the people use credit and pay once a month so I created this  PWA app to help small business manage account instead of writing everything in aNotebook .
-            To use and demo this app please use this password:  <span className="inportan">123456</span>  and user account name: <span className="inportan">TAVERAS78@GMAIL.COM</span> .
-            </p>
-        </div>
-   
-        <div className="MyProjects__tec">
-          <Chip color="primary"label="node.js" />
-          <Chip color="primary"label="angular" />
-          <Chip color="primary"label="material" />
-          <Chip color="primary"label="bootstrap" />
-          <Chip color="primary"label="mongodb" />
-          <Chip color="primary"label="PWA" />
-        </div>
-        
-        <div className="iconlist">
-          <a href="https://github.com/kamekazz/fiobook-cli"rel="noopener noreferrer" target="_blank"><Fab className="fab" color="primary"  >
-              <i className="fab fa-github "></i>
-            </Fab>
-          </a>
+      <div onScroll={this.activet} className="MyProjects">
+        <div className="mainConten">
 
-          <a href="https://colbook-0818.firebaseapp.com/login"rel="noopener noreferrer" target="_blank"><Fab className="fab" color="primary"  >
-          <i className="fas fa-link"></i>
-            </Fab>
-          </a>
-        </div>
-    
-        <figure className="MyProjects__shape--2">
-            <img src="ast/img/music.jpg" alt="Person on a tour" className="MyProjects__img" />
-            <figcaption className="MyProjects__caption">Viva la Musica</figcaption>
-        </figure>
-        <h3 className="MyProjects__h3--2">Viva la Musica</h3>
-
-        <div className="iconlist--2">
-          <a href="https://github.com/kamekazz/viva-cli"rel="noopener noreferrer" target="_blank"><Fab className="fab" color="primary"  >
-              <i className="fab fa-github "></i>
-            </Fab>
-          </a>
-
-          <a href="https://viva-la-musica.firebaseapp.com/signin"rel="noopener noreferrer" target="_blank"><Fab className="fab" color="primary"  >
-          <i className="fas fa-link"></i>
-            </Fab>
-          </a>
-        </div>
-
-        <div className="MyProjects__text-2">
-            <p>
-            I created this app for a mall It gives the ability of every guess to add music to a playlist And be able to vote for the next song, This is not the original app I changed a couple things to be suitable for the web .To demo this app please use this password: <span className="inportan">demo</span>  and user account name: <span className="inportan">demo</span> .
-            </p>
-        </div>
-
-        <div className="MyProjects__tec-2">
-          <Chip color="primary"label="node.js" />
-          <Chip color="primary"label="React js" />
-          <Chip color="primary"label="material" />
-          <Chip color="primary"label="mongodb" />
-        </div>
-
-
-
-        <div className="btn--posich" >
-        <a href="https://codepen.io/kamekazz/pens/popular/"rel="noopener noreferrer" target="_blank">
-            <div  className="gettouch btn"><i className="fab fa-codepen"></i> codepen</div>
-        </a>
-        </div>
-
-        
-          <div className="btn--posich-2" >
-            <a href="https://github.com/kamekazz"rel="noopener noreferrer" target="_blank">
-              <div  className="gettouch btn"><i className="fab fa-github "></i> more Projects</div>
-            </a>
-          </div> 
-      
-        <div className="btn--posich-3" >
-        <a href="https://codesandbox.io/u/kamekazz"rel="noopener noreferrer" target="_blank">
-            <div  className="gettouch btn"><i className="fab fa-codepen"></i> codesandbox</div>
-        </a>
-        </div> 
-
-       <Slide direction="left" in={onViw} timeout={1000} >
-          <div className="title-p">
-            <div className="title">My Projects</div>
+          <div className="prolist">
+            {rdpro()}
           </div>
-        </Slide>
 
-        <Slide direction="right" in={onViw} timeout={3000} >
-            <div className="s2-bg"></div>
-        </Slide>
+
+        </div>
       </div>
     )
   }
